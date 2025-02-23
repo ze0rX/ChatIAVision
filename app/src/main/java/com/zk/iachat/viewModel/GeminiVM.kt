7e48dd4a7fd8ef83753746ccfbbfa2b0a2088bc5
zk <system32x86@outlook.com>
2025-02-23 15:15:03 -0600

    include justificacion

--- app/src/main/java/com/zk/iachat/viewModel/GeminiVM.kt
+++ app/src/main/java/com/zk/iachat/viewModel/GeminiVM.kt
@@ -6,10 +6,7 @@ import android.net.Uri
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.room.Room
